c567380b89c7cd9425e4cf9450949df1e19e7414
Lukasz Zygadlo <lukasz.zygadlo@gmail.com>
2017-05-23 15:43:42 +0100

    deleting name column from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   def self.find_for_facebook_oauth(auth)
     user_params = auth.slice(:provider, :uid)
     user_params.merge! auth.info.slice(:email, :first_name, :last_name)
-    byebug
     user_params[:facebook_picture_url] = auth.info.image
     user_params[:token] = auth.credentials.token
     user_params[:token_expiry] = Time.at(auth.credentials.expires_at)
