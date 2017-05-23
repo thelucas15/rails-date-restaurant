dadc660f2f13cae2bc26f8ac7c752f28b9d6693a
Lukasz Zygadlo <lukasz.zygadlo@gmail.com>
2017-05-23 15:15:35 +0100

    make steps from slides fb-connect

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522222606) do
+ActiveRecord::Schema.define(version: 20170523134208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 20170522222606) do
     t.string "gender_pref"
     t.text "description"
     t.string "hash_tag_pref"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
