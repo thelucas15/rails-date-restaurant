8e01c12b95473389bc6d1a3ec865e37faaccfd4f
Lukasz Zygadlo <lukasz.zygadlo@gmail.com>
2017-05-24 13:33:54 +0100

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,16 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :restaurants, only: [:index, :show] do
-    resources :match_lists, only: [:index, :show, :new, :create, :update] do
-      resources :evaluations, only: [:show] do
-        resources :reservations, only: [:show] do
-        end
-      end
-    end
+    resources :match_lists, only: [:index, :show, :new, :create, :update]
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :match_lists do
+    resources :evaluations, only: [:show] do
+  end
 
+  resources :evaluations, only: [:show] do
+    resources :reservations, only: [:show] do
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
