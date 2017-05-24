3d4323fb7c391d1e0be6862a7cbd3a4868f80fb9
ricardo pinto <rcostapinto@gmail.com>
2017-05-24 16:52:40 +0100

    Page static About

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about'
+
+  get 'contact', to: 'pages#contact'
+
   devise_for :users,
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
