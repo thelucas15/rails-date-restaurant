f25d62620da0fd8c26f987acc369d70a4e409097
George Cubas <georgeraulc@gmail.com>
2017-05-25 17:29:21 +0100

    Done with basic Show of Restaurants with added features

--- app/controllers/evaluations_controller.rb
+++ app/controllers/evaluations_controller.rb
@@ -1,2 +1,6 @@
 class EvaluationsController < ApplicationController
+
+  def new
+    Evaluation.new
+  end
 end
--- app/models/match_list.rb
+++ app/models/match_list.rb
@@ -3,4 +3,8 @@ class MatchList < ApplicationRecord
   belongs_to :user
   validates :start_time, presence: true
   validates :date, presence: true
+  validates :restaurant_id, uniqueness: { scope: [:user_id, :date], message: 'You  cant book the same restaurant on the same day' }
+
 end
+
+
