70bf555287ff54fe034406344abc22586e858b3f
ricardo pinto <rcostapinto@gmail.com>
2017-05-25 18:47:53 +0100

    add new photo image to the background

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,13 @@ class PagesController < ApplicationController
   def home
     @image = [
       "rachael-gorjestani-154908.jpg",
-      "brooke-lark-203842.jpg",
-      "brooke-lark-209712.jpg",
       "dan-gold-105699.jpg",
-      "eaters-collective-219711.jpg",
       "matthew-henry-86779.jpg",
       "matthew-henry-86779.jpg",
       "seemi-samuel-15564.jpg",
       "serge-esteve-5821.jpg",
       "tegan-mierle-157974.jpg",
+      "ali-inay-9858.jpg"
     ].sample
   end
 
