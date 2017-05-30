5722aea068b9f9abe800954f8809b3f41793faad
George Cubas <georgeraulc@gmail.com>
2017-05-30 11:15:23 +0100

    done with basic pundit implementation for restaurant and matchlist controllers

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,25 +7,23 @@ class RestaurantsController < ApplicationController
     @food_type = params[:food_type]
     @date = session[:date] = params[:date]
     @start_time = session[:start_time] = params[:start_time]
-    @restaurants = Restaurant.where(food_type: @food_type)
+
+    @restaurants = policy_scope(Restaurant).where(food_type: @food_type)
+
   end
 
   def show
 
-    @hash_tag_pref = session[:hash_tag_pref]
 
     @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
     @match_list = MatchList.new()
     @date = session[:date]
     @start_time = session[:start_time]
     @hash_tag_pref = current_user.hash_tag_pref
     #@user_in_list = @restaurant.match_lists.any? {|i| i.user == current_user}
-
     @user_match_list = MatchList.where(user_id: current_user.id, restaurant_id: @restaurant.id)
-
   end
 
-  def search
 
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,9 @@ Rails.application.routes.draw do
     resources :reservations, only: [:show, :create, :new] do
     end
   end
+  resources :users, only: [:index, :edit, :show, :create] do
+    post :impersonate, on: :member
+    post :stop_impersonating, on: :collection
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
