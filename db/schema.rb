abf4038275e29e9d7dafb730e8d05d9b8d8caf7c
ricardo pinto <rcostapinto@gmail.com>
2017-05-30 15:32:46 +0100

    Solved seed problem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529123001) do
+ActiveRecord::Schema.define(version: 20170530143200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20170529123001) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.integer "age"
     t.integer "age_pref_start"
     t.integer "age_pref_end"
