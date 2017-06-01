c38ce88d8e29b5919edf13765958ffdd402c7cde
George Cubas <georgeraulc@gmail.com>
2017-06-01 17:40:19 +0100

    added chat with action cable but it dont work looked through it anal retentively but nothing

--- app/models/match_list.rb
+++ app/models/match_list.rb
@@ -3,7 +3,7 @@ class MatchList < ApplicationRecord
   belongs_to :user
   validates :start_time, presence: true
   validates :date, presence: true
-  validates :restaurant_id, uniqueness: { scope: [:user_id, :date], message: 'You  cant book the same restaurant on the same day' }
+ # validates :restaurant_id, uniqueness: { scope: [:user_id, :date], message: 'You  cant book the same restaurant on the same day' }
 
 end
 
