720f978ae9f414d57c13fab60009d5f0be55ae9b
Lukasz Zygadlo <lukasz.zygadlo@gmail.com>
2017-06-03 09:36:08 +0100

    slider with value

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -6,13 +6,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
 
-  process eager: true  # Force version generation at upload time.
-  process convert: 'jpg'
-  process resize_to_fill: [150,150]
-
-
   version :thumb do
-
+    process eager: true  # Force version generation at upload time.
+    process convert: 'jpg'
+    process resize_to_fill: [150,150]
     cloudinary_transformation effect: "brightness:30", radius: 20,
       width: 150, height: 150, crop: :thumb, gravity: :face
   end
