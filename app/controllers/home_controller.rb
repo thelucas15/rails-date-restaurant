de6847a27f0bb7e9fc3164e35cd4e462896bd4ea
George Cubas <georgeraulc@gmail.com>
2017-06-03 18:35:25 +0100

    slight changes to master

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
    def index
     session[:conversations] ||= []
 
-    @users = User.online_now.where.not(id: current_user)
+    @users = User.all.where.not(id: current_user)
 
     @conversations = Conversation.includes(:recipient, :messages, )
                                  .find(session[:conversations])
