d66a919cb0d52901d8e2080b8e76cb14684755ea
Lukasz Zygadlo <lukasz.zygadlo@gmail.com>
2017-06-03 12:47:48 +0100

    changes to uploader

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -6,11 +6,24 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   include Cloudinary::CarrierWave
 
-  version :thumb do
-    process eager: true  # Force version generation at upload time.
-    process convert: 'jpg'
-    process resize_to_fill: [150,150]
+  process eager: true  # Force version generation at upload time.
+
+  process convert: 'jpg'
+
+  version :standard do
+    resize_to_fit 800, 600
+  end
+
+  version :bright_face do
     cloudinary_transformation effect: "brightness:30", radius: 20,
       width: 150, height: 150, crop: :thumb, gravity: :face
   end
+
+  # version :thumb do
+  #   process eager: true  # Force version generation at upload time.
+  #   process convert: 'jpg'
+  #   process resize_to_fill: [150,150]
+  #   cloudinary_transformation effect: "brightness:30", radius: 20,
+  #     width: 150, height: 150, crop: :thumb, gravity: :face
+  # end
 end
