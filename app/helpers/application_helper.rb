036586e0697cb7075a2d4c466c808c5b8a660c6a
mariana santos <mymarysaints@gmail.com>
2017-06-05 12:12:21 +0100

    changes on image of avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,29 @@ def resource_name
 
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
-end
+  end
+
+  def avatar_url(user)
+
+    if user.facebook_picture_url
+      user.facebook_picture_url
+    elsif user.photo.url
+      user.photo.url
+    else
+      "http://placehold.it/30x30"
+    end
+
+
+    # current_user.facebook_picture_url
+
+    # if !avatar_url.blank?
+    #   image_tag avatar_url, class: "avatar dropdown-toggle", id: "navbar-wagon-menu", "data-toggle" => "dropdown"
+    # elsif !current_user.photo.url.blank?
+    #   cl_image_tag current_user.photo.url(:bright_face), class: "avatar dropdown-toggle", id: "navbar-wagon-menu", "data-toggle" => "dropdown"
+    # else
+    #   image_tag "http://placehold.it/30x30"
+    # end
+
+  end
 
 end
